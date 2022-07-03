939bc6662d63cf0f054c00d4207201201f2d7761
DivisonOfficer <wlssud0701@naver.com>
2022-07-03 18:18:41 +0900

    [07.03] Clock.js 코드일부정리

--- src/components/transaction/Clock.js
+++ src/components/transaction/Clock.js
@@ -40,18 +40,18 @@ export default function Clock() {
 
   const LiveTimeContainer = () => {
     const [minutes, setMinutes] = useState(59);
-    const [seconds, setSeconds] = useState(0);
+    const [seconds, setSeconds] = useState(59);
 
     useEffect(() => {
       const countdown = setInterval(() => {
-        if (parseInt(seconds) > 0) {
-          setSeconds(parseInt(seconds) - 1);
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
         }
-        if (parseInt(seconds) === 0) {
-          if (parseInt(minutes) === 0) {
+        if (seconds === 0) {
+          if (minutes === 0) {
             clearInterval(countdown);
           } else {
-            setMinutes(parseInt(minutes) - 1);
+            setMinutes(minutes - 1);
             setSeconds(59);
           }
         }
@@ -59,9 +59,13 @@ export default function Clock() {
       return () => clearInterval(countdown);
     }, [minutes, seconds]);
 
+    const numPadding = (num)=>{
+      return num < 10 ? `0${num}` : num;
+    }
+
     return (
       <>
-        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        {minutes}:{numPadding(seconds)}
       </>
     );
   };
