cd865bf10740f51ad78e9dfe2eb5fb5736192cf0
DivisonOfficer <41609506+DivisonOfficer@users.noreply.github.com>
2022-07-10 22:58:01 +0900

    [07.10] winjob

--- src/components/mypoint/TransactionTable.js
+++ src/components/mypoint/TransactionTable.js
@@ -1,8 +1,6 @@
-import kakaoTalk from "../../assets/kakaoTalk.png";
-import { HStack, Text, VStack, Flex } from "@chakra-ui/layout";
 import styled from "styled-components";
-import { Button, Link } from "@chakra-ui/react";
-import { useState } from "react";
+import SendItem from './SendItem'
+
 
 
 const Table = styled.table`
@@ -59,64 +57,7 @@ const DisplayTable = ({type, transData}) =>{
 
 
 
-const SendItem = ({transData, type}) =>{
 
-    const width = 100/ (type ? 5 : 6);
-
-
-    const [focus, setFocus] = useState(false);
-
-    return(<tr
-      onPointerEnter={()=>setFocus(true)} onPointerLeave = {()=>setFocus(false)}
-      bgColor = {focus && "#bbbbee" }
-    >
-      <SourceView  width = {width + '%'} icon={kakaoTalk} text={"KakaoPay"}/>
-      <TextView width = {width + '%'}title = "Date" content = {transData.date} focus = {focus}/>
-      { type !== "from" && (<TextView width = {width + '%'} title="To" content = {transData.to} focus = {focus}/>)    }
-      { type !== "to" && (<TextView width = {width + '%'} title="From" content = {transData.from} focus = {focus}/>)    }
-      {/* <Td>{ipfs[i]}</Td> */}
-      <TextView width = {width + '%'} title = "Amount" content = {transData.value} focus = {focus}/>
-      <BtnHash width = {width + '%'} link = {Link} text = {transData.hash} focus = {focus}/>
-    </tr>);
-  }
-
-
-  const TextView = ({title, content, focus}) =>{
-
-    return(
-      <Td >
-
-    <Flex justifyContent="center">
-      <VStack align = "start" width = "60%" justifyContent="start" >
-        <Text fontSize = 'sm' color={focus ? "white" : "#cecece"}>{title}</Text>
-        <Text fontSize='lg'
-
-        >{content}</Text>
-      </VStack>
-      </Flex>
-    </Td>
-    )
-  }
-
-  const SourceView = ({icon, text}) =>{
-
-    return(
-    <Td>
-      <HStack justifyContent="center">
-        <Icon src={icon} />
-        <Text>{text}</Text>
-      </HStack>
-    </Td>);
-  }
-
-  const BtnHash = ({link, text, focus}) =>{
-
-    return(<Td>
-      <Button colorScheme='blue' size="md" variant = {focus ? 'solid': 'outline'} as={link} isExternal>
-        Open Hash
-      </Button>
-    </Td>);
-  }
 
 
 
