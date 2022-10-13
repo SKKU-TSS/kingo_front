7ce6811c27c64d82bf74494a980611e7c1af88b3
DivisonOfficer <wlssud0701@naver.com>
2022-10-13 10:23:45 +0900

    Header content type

--- src/remote/PendingTransaction.js
+++ src/remote/PendingTransaction.js
@@ -1,74 +1,63 @@
-import axios from "axios"
-import { Cookies } from "react-cookie"
-import { COOKIE_TOKEN } from "../CookieConst"
-import BACKEND_URL from "../ServerConst"
-import commonHeader from "./commonHeader"
+import axios from "axios";
+import { Cookies } from "react-cookie";
+import { COOKIE_TOKEN } from "../CookieConst";
+import BACKEND_URL from "../ServerConst";
+import commonHeader from "./commonHeader";
 
+const getRecentData = (onSuccess, onFailed) => {
+  const header = commonHeader();
+  console.log(header);
 
-const getRecentData = (
-    onSuccess, onFailed
-)=>{
-
-    const header = commonHeader()
-    console.log(header)
-
-
-    axios({
-        url: `${BACKEND_URL}/api/transaction/nonpending/get`,
-        method: 'get',
-        headers : header
-    }).then((response)=>{
-        if(response.status === 200)
-            onSuccess(response.data);
-        else onFailed(response)
-    }).catch((error)=>{
-
-        console.log(error);
-        onFailed(error);
+  axios({
+    url: `${BACKEND_URL}/api/transaction/nonpending/get`,
+    method: "get",
+    headers: header,
+  })
+    .then((response) => {
+      if (response.status === 200) onSuccess(response.data);
+      else onFailed(response);
     })
+    .catch((error) => {
+      console.log(error);
+      onFailed(error);
+    });
+};
 
-}
+const getPendingData = (onSuccess, onFailed) => {
+  const header = commonHeader();
+  console.log(header);
 
-const getPendingData = (
-    onSuccess, onFailed
-)=>{
-
-    const header = commonHeader()
-    console.log(header)
-
-
-    axios({
-        url: `${BACKEND_URL}/api/transaction/pending/get`,
-        method: 'get',
-        headers : header
-    }).then((response)=>{
-        console.log(response)
-        if(response.status === 200)
-        onSuccess(response.data);
-    }).catch((error)=>{
-
-        console.log(error);
-        onFailed(error);
+  axios({
+    url: `${BACKEND_URL}/api/transaction/pending/get`,
+    method: "get",
+    headers: header,
+  })
+    .then((response) => {
+      console.log(response);
+      if (response.status === 200) onSuccess(response.data);
     })
+    .catch((error) => {
+      console.log(error);
+      onFailed(error);
+    });
+};
 
-}
+const approvePendingData = (id, onSuccess, onFailed) => {
+  const header = commonHeader();
 
-const approvePendingData = (id, onSuccess, onFailed) =>{
-    const header = commonHeader()
+  axios({
+    url: `${BACKEND_URL}/api/transaction/pending/assign`,
+    method: "post",
+    headers: header,
+    data: {
+      id: id,
+    },
+  })
+    .then(onSuccess)
+    .catch((error) => {
+      console.log(error);
+      onFailed(error);
+    });
+};
 
-    axios({
-        url : `${BACKEND_URL}/api/transaction/pending/assign`,
-        method : 'post',
-        headers : header,
-        data : {
-            id : id
-        }
-    }).then(onSuccess).catch((error)=>{
-        console.log(error)
-        onFailed(error)
-    })
-
-}
-
-export { getPendingData, getRecentData,
-approvePendingData }
+export { getPendingData, getRecentData, approvePendingData };
--- src/remote/commonHeader.js
+++ src/remote/commonHeader.js
@@ -1,13 +1,12 @@
-import { useCookies, Cookies, CookiesProvider } from "react-cookie"
-import { COOKIE_TOKEN } from "../CookieConst"
+import { useCookies, Cookies, CookiesProvider } from "react-cookie";
+import { COOKIE_TOKEN } from "../CookieConst";
 
-
-const commonHeader = ()=>{
-
-    const cookieStore = new Cookies()
-    return {
-        Authorization : `Bearer ${cookieStore.get(COOKIE_TOKEN)}`
-    }
-}
+const commonHeader = () => {
+  const cookieStore = new Cookies();
+  return {
+    Authorization: `Bearer ${cookieStore.get(COOKIE_TOKEN)}`,
+    "Content-Type": `application/json`,
+  };
+};
 
 export default commonHeader;
