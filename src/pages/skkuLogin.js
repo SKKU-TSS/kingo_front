e7fe1db75c9dd994e10bd2d89c3641e07499b3ab
DivisonOfficer <wlssud0701@naver.com>
2022-10-13 14:44:57 +0900

    이메일 검정

--- src/pages/skkuLogin.js
+++ src/pages/skkuLogin.js
@@ -1,134 +1,138 @@
-import { HStack, Input, VStack, Text, Button, Box, InputGroup, InputLeftAddon } from "@chakra-ui/react"
-import { useCallback, useEffect, useState } from "react"
-import { actionSkkuLogin } from "../redux/skkusign/action"
-import { connect, useDispatch } from 'react-redux'
-import { useCookies } from "react-cookie"
-import { COOKIE_EMAIL } from "../CookieConst"
-import axios from "axios"
-import BACKEND_URL from "../ServerConst"
-import skkuLogin from "../remote/SkkuLogin"
+import {
+  HStack,
+  Input,
+  VStack,
+  Text,
+  Button,
+  Box,
+  InputGroup,
+  InputLeftAddon,
+} from "@chakra-ui/react";
+import { useCallback, useEffect, useState } from "react";
+import { actionSkkuLogin } from "../redux/skkusign/action";
+import { connect, useDispatch } from "react-redux";
+import { useCookies } from "react-cookie";
+import { COOKIE_EMAIL } from "../CookieConst";
+import axios from "axios";
+import BACKEND_URL from "../ServerConst";
+import skkuLogin from "../remote/SkkuLogin";
 
+const SKKULoginPage = () => {
+  const dispatch = useDispatch();
 
+  const [form, setForm] = useState({ id: "", pw: "" });
 
-const SKKULoginPage = () =>{
+  const login = () => {
+    let domain = form.id.split("@")[1];
 
-    const dispatch = useDispatch();
-
-    const [form, setForm] = useState({id:"", pw : ""})
-
-    const login = ()=>{
-        skkuLogin(
-            form.id,
-            (response)=>{
-                alert("메일을 전송했습니다.")
-            },
-            (error)=>{
-                alert("메일 전송에 실패했습니다.")
-
-            }
-
-        )
-
-        //dispatch(actionSkkuLogin(form.id));
-    }
-
-    const fieldStyle = {
-        borderWidth : "1px",
-        borderRadius : "4px",
-        borderColor : "#AFAFAF",
-        backgroundColor : "white",
-       
-        width : "300px"
-    }
-
-
-    const [emailCookie, setEmailCookie, removeEmailCookie] = useCookies(COOKIE_EMAIL);
-
-    const updateForm = (title, value)=>{
-        //setEmailCookie(COOKIE_EMAIL , value);
-        setSavedEmail(value);
-        setForm({...form,
-                id : value
-            });
-
-        
-        
-    }
-
-    const [savedEmail, setSavedEmail] = useState(null);
-
-    useEffect(()=>{
-        
-        if(emailCookie !== undefined)
-        {
-            setSavedEmail(emailCookie.LOGIN_EMAIL);
+    if (domain === "skku.edu" || domain === "g.skku.edu")
+      skkuLogin(
+        form.id,
+        (response) => {
+          alert("메일을 전송했습니다.");
+        },
+        (error) => {
+          alert("메일 전송에 실패했습니다.");
         }
+      );
+    else alert("성균관대학교 이메일이 아닙니다.");
 
-    },[]);
-    
-    //if(emailCookie !== undefined)
-    //    savedText = emailCookie.LOGIN_EMAIL
+    //dispatch(actionSkkuLogin(form.id));
+  };
 
-    return(<VStack align="center" justifyContent="center" width="100%" spacing = {8}>
-        <Header/>
-        <TextField title = "성균인 계정" placeholder="user@skku.edu" savedText = {savedEmail} setForm = {updateForm} width = "300px" />
-        <BtLogin onClick={login}/>
-    </VStack>)
-}
+  const fieldStyle = {
+    borderWidth: "1px",
+    borderRadius: "4px",
+    borderColor: "#AFAFAF",
+    backgroundColor: "white",
 
+    width: "300px",
+  };
 
-const Header = ()=>{
+  const [emailCookie, setEmailCookie, removeEmailCookie] =
+    useCookies(COOKIE_EMAIL);
 
-    const url = "https://www.skku.edu/_res/skku/img/skku_s.png"
+  const updateForm = (title, value) => {
+    //setEmailCookie(COOKIE_EMAIL , value);
+    setSavedEmail(value);
+    setForm({ ...form, id: value });
+  };
 
+  const [savedEmail, setSavedEmail] = useState(null);
 
-    return(<Box
-        bgImage={`url('${url}')`}
-        bgPosition="center"
-        bgRepeat="no-repeat"
-        height='200px'
-        width = '200px'
-        bgSize = 'cover'
-      />)
-}
+  useEffect(() => {
+    if (emailCookie !== undefined) {
+      setSavedEmail(emailCookie.LOGIN_EMAIL);
+    }
+  }, []);
 
+  //if(emailCookie !== undefined)
+  //    savedText = emailCookie.LOGIN_EMAIL
 
-const TextField = ({title,placeholder, savedText, setForm, onChange}) =>{
+  return (
+    <VStack align="center" justifyContent="center" width="100%" spacing={8}>
+      <Header />
+      <TextField
+        title="성균인 계정"
+        placeholder="user@skku.edu"
+        savedText={savedEmail}
+        setForm={updateForm}
+        width="300px"
+      />
+      <BtLogin onClick={login} />
+    </VStack>
+  );
+};
 
-    const [textValue, setTextValue] = useState(null)
+const Header = () => {
+  const url = "https://www.skku.edu/_res/skku/img/skku_s.png";
 
-    useEffect(()=>{
-        setTextValue(savedText);
-    });
+  return (
+    <Box
+      bgImage={`url('${url}')`}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      height="200px"
+      width="200px"
+      bgSize="cover"
+    />
+  );
+};
 
+const TextField = ({ title, placeholder, savedText, setForm, onChange }) => {
+  const [textValue, setTextValue] = useState(null);
 
-    
-    const setValue = useCallback((value)=>{
-        setForm(title,value);
-    },[])
+  useEffect(() => {
+    setTextValue(savedText);
+  });
 
-    return(
-    
-    <InputGroup width = '300px' size='sm'>
-        <InputLeftAddon children = {title}/>
-        <Input value={textValue} placeholder={placeholder} size='xl' background = 'white' onChange={(event)=>
-            {
-                setTextValue(event.target.value);
-            setValue(event.target.value);
-            }
-            }/>
+  const setValue = useCallback((value) => {
+    setForm(title, value);
+  }, []);
+
+  return (
+    <InputGroup width="300px" size="sm">
+      <InputLeftAddon children={title} />
+      <Input
+        value={textValue}
+        placeholder={placeholder}
+        size="xl"
+        background="white"
+        onChange={(event) => {
+          setTextValue(event.target.value);
+          setValue(event.target.value);
+        }}
+      />
     </InputGroup>
-    )
-}
+  );
+};
 
-const BtLogin = ({onClick})=>{
-    return(<Button colorScheme='teal' onClick={onClick}
-    fontWeight='400'>
-    메일 보내기
-  </Button>)
-}
-
-
-
+const BtLogin = ({ onClick }) => {
+  return (
+    <Button colorScheme="teal" onClick={onClick} fontWeight="400">
+      메일 보내기
+    </Button>
+  );
+};
 
 export default SKKULoginPage;
