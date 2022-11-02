770e45ff741e06c4766c34b7456ae9226b451600
DivisonOfficer <wlssud0701@naver.com>
2022-11-02 19:04:32 +0900

    [11.02]

--- src/components/mypoint/MyPointAll.js
+++ src/components/mypoint/MyPointAll.js
@@ -9,77 +9,18 @@ import axios from "axios";
 import getAllTransData from "../../remote/TransactionAll";
 import TransactionTable from "./TransactionTable";
 
-function bindReceiveData(sendList, setSendData, setIpfs){
-  
-  const urlList = [];
-
-  setSendData(sendList);
-
-  
-  for (let i = 0; i < sendList.length; i++) {
-    axios
-      .get(`http://localhost:5000/api/result/${sendList[i].hash}`)
-      .then((res) => urlList.push(res.data));
-  }
-  setIpfs(urlList);
-}
-
-async function bindData(account, setReceiveData, setIpfs)
+async function bindData(account, setReceiveData)
 {
-  return
+
     await getAllTransData(account, (sendData) =>{
-      // console.log(response.data[1]._from);
-      bindReceiveData(sendData,setReceiveData,setIpfs)
+      setReceiveData(sendData.body)
     });
 }
 
 export default function MyTransactionsAll() {
   // 나와 관련된 모든 트랜잭션을 볼 수 있는 컴포넌트
-
-
-  const [ipfs, setIpfs] = useState([]);
-
   const [transData, setTransData] = useState([
-      {
-          from : "온라인 명륜당",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "100",
-          date : "2021. 07. 12",
-          hash : "hashid",
-      },
-      {
-          from : "온라인 멘토링",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "100",
-          date : "2021. 09. 22",
-          hash : "hashid",
-      },
-      {
-          from : "킹고인과의 만남",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "100",
-          date : "2022. 5. 6",
-          hash : "hashid",
-      },
-      {
-          from : "온라인 멘토링",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "150",
-          date : "2022. 07. 12",
-          hash : "hashid",
-      },
-      {
-          from : "성균색 에세이",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "100",
-          date : "2022. 07. 21",
-          hash : "hashid",
-      },
+
 
 
   ]);
--- src/remote/TransactionAll.js
+++ src/remote/TransactionAll.js
@@ -4,7 +4,50 @@ import handleTransaction from "./response/HandleTransaction";
 
 function getAllTransData(account, callback)
 {
-    
+    callback({
+        body : [{
+            from : "온라인 명륜당",
+            to : "myungrun@skku.edu",
+            type : "",
+            value : "100",
+            date : "2021. 07. 12",
+            hash : "hashid",
+        },
+                {
+                    from : "온라인 멘토링",
+                    to : "myungrun@skku.edu",
+                    type : "",
+                    value : "100",
+                    date : "2021. 09. 22",
+                    hash : "hashid",
+                },
+                {
+                    from : "킹고인과의 만남",
+                    to : "myungrun@skku.edu",
+                    type : "",
+                    value : "100",
+                    date : "2022. 5. 6",
+                    hash : "hashid",
+                },
+                {
+                    from : "온라인 멘토링",
+                    to : "myungrun@skku.edu",
+                    type : "",
+                    value : "150",
+                    date : "2022. 07. 12",
+                    hash : "hashid",
+                },
+                {
+                    from : "성균색 에세이",
+                    to : "myungrun@skku.edu",
+                    type : "",
+                    value : "100",
+                    date : "2022. 07. 21",
+                    hash : "hashid",
+                },]
+    })
+    return
+
     return axios
     .get(`${BACKEND_URL}api/transaction?who=to&address=${account}`).then(
         function(response){
