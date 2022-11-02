7d1325bdfe7f701da5140bc5e4acf8b5c276e168
DivisonOfficer <wlssud0701@naver.com>
2022-11-02 19:00:08 +0900

    [22.11.02] 전체 내역 통신 흐름 준비

--- src/components/mypoint/MyPointReceive.js
+++ src/components/mypoint/MyPointReceive.js
@@ -12,82 +12,27 @@ import TransactionTable from "./TransactionTable";
 
 
 
-function bindReceiveData(sendList, setSendData, setIpfs){
-  
-  const urlList = [];
-
-  setSendData(sendList);
-
-  for (let i = 0; i < sendList.length; i++) {
-    axios
-      .get(`http://localhost:5000/api/result/${sendList[i].hash}`)
-      .then((res) => urlList.push(res.data));
-  }
-  setIpfs(urlList);
-}
-
-async function bindData(account, setReceiveData, setIpfs)
+async function bindData(account, setReceiveData)
 {
-  return
     await getRecieveData(account, (sendData) =>{
-      // console.log(response.data[1]._from);
-      bindReceiveData(sendData,setReceiveData,setIpfs)
+      setReceiveData(sendData.body)
     });
 }
 
 export default function MyTransactionsReceive() {
   // 내가 포인트를 받는 트랜잭션을 볼 수 있도록 한 컴포넌트
-  const [ipfs, setIpfs] = useState([]);
+
+
 
   const [receiveData, setReceiveData] = useState([
-      {
-          from : "온라인 명륜당",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "100",
-          date : "2021. 07. 12",
-          hash : "hashid",
-      },
-      {
-          from : "온라인 멘토링",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "100",
-          date : "2021. 09. 22",
-          hash : "hashid",
-      },
-      {
-          from : "킹고인과의 만남",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "100",
-          date : "2022. 5. 6",
-          hash : "hashid",
-      },
-      {
-          from : "온라인 멘토링",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "150",
-          date : "2022. 07. 12",
-          hash : "hashid",
-      },
-      {
-          from : "성균색 에세이",
-          to : "myungrun@skku.edu",
-          type : "",
-          value : "100",
-          date : "2022. 07. 21",
-          hash : "hashid",
-      },
-      
+
   ]);
 
 
   const { account } = useWeb3React();
 
   useEffect(() => {
-    bindData(account,setReceiveData,setIpfs);
+      bindData(account,setReceiveData);
   }, []);
   
   return (<TransactionTable type="from" transData={receiveData}/> );
