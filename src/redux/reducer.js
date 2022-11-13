fc50ecb5f2e45b23c4ba28e44d338e42a8ef4f03
DivisonOfficer <wlssud0701@naver.com>
2022-11-13 18:23:21 +0900

    [11.13] init ipfs reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import skkuLogin from "./skkusign/reducer";
 import recentDashboard from "./dashboardTable/dashbaordTable";
 import { totalBalanceReducer } from "./dashboardTable/totalBalance";
+import { ipfsReducer} from "./dashboardTable/ipfs";
 
 const rootReducer = combineReducers({
   recentDashboard: recentDashboard,
   skkuLogin: skkuLogin,
   totalBalance: totalBalanceReducer,
+    ipfs : ipfsReducer
 });
 
 export default rootReducer;
